41dc801c1c5fba9cbcf3723608c411004bbf0429
mikoslaf <mikolaj2003wojcik@gmail.com>
2025-01-25 09:46:28 +0100

    Implement category filtering in filters component; add product management methods in service

--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../products.service';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-filters',
   standalone: true,
-  imports: [],
+  imports: [NgFor, FormsModule],
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.scss'
 })
 export class FiltersComponent {
 
+  get categoryList(): Array<string> {
+    return this.product.categories;
+  }
+  
+  get selectedCategory(): Record<string, boolean> {
+    return this.product.selectedCategory;
+  }
+
+  set selectedCategory(value: Record<string, boolean>) {
+    this.product.selectedCategory = value;
+  }
+
+  private lastStan: boolean = false;
+
+  public reversFilters(): void {
+    this.product.categories.forEach((category) => {
+      this.selectedCategory[category] = !this.lastStan;
+    });
+    this.lastStan = !this.lastStan;
+  }
+
+  constructor(private product: ProductsService){
+    this.product.categories.forEach((category) => {
+      this.selectedCategory[category] = false;
+    });
+  }
 }
--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Product } from './product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
+  public selectedCategory: Record<string, boolean> = {};
   public categories: Array<string> = ["owoce", "warzywa", "nabiał", "mięso", "pieczywo", "słodycze", "napoje"];
+  public products: Product[] = [];
+
+  public addProduct(name: string, category: string): void {
+    let product: Product = this.products.filter((pr) => pr.name == name)[0];
+    if(product){
+      product.selected = true;
+      product.category = category;
+    } else {
+      product = {name: name, category: category, selected: true};
+      this.products.push(product);
+    }
+  }
+
+  public removeProduct(name: string): void {
+    this.products = this.products.filter((pr) => pr.name != name);
+  }
+
   constructor() { }
 }
