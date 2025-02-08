473fc9f9778a19d949c87da39620f1093e8ddfe3
mikoslaf <mikolaj2003wojcik@gmail.com>
2025-02-08 09:51:01 +0100

    Implement product addition functionality with category selection

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,19 +1,30 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../products.service';
 import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, FormsModule],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.scss'
 })
 export class AddProductComponent {
 
+  public name: string = "";
+  public category: string = "";
+
   constructor(public products: ProductsService) {}
 
   public firstCharToUpper(word: string): string {
     return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+
+  public addProduct(): void {
+    console.log("Adding product: " + this.name + "| category: " + this.category);
+    if(this.name != "" && this.category != ""){
+      this.products.addProduct(this.name, this.category);
+    }
   }
 }
--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -5,10 +5,14 @@ import { Product } from './product';
   providedIn: 'root'
 })
 export class ProductsService {
-
+  
   public selectedCategory: Record<string, boolean> = {};
   public categories: Array<string> = ["owoce", "warzywa", "nabiał", "mięso", "pieczywo", "słodycze", "napoje"];
   public products: Product[] = [];
+  
+  constructor() {
+    this.load();
+   }
 
   public addProduct(name: string, category: string): void {
     let product: Product = this.products.filter((pr) => pr.name == name)[0];
@@ -19,11 +23,27 @@ export class ProductsService {
       product = {name: name, category: category, selected: true};
       this.products.push(product);
     }
+    this.save();
   }
 
   public removeProduct(name: string): void {
     this.products = this.products.filter((pr) => pr.name != name);
+    this.save();
   }
 
-  constructor() { }
+  public save(): void {
+    localStorage.setItem('sem3_przep_products', JSON.stringify(this.products));
+  }
+
+  public load(): void {
+    if(!localStorage){
+      return;
+    }
+    
+    const saved = localStorage.getItem('sem3_przep_products');
+    if(saved){
+      this.products = JSON.parse(saved);
+    }
+  }
+
 }
